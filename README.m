1473544bf985e013b3bf6e6fcbf8889244322ef5
Topaz <nafula.charity355@gmail.com>
2024-08-27 10:19:13 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,12 @@
+- 👋 Hi, I’m @C-topaz
+- 👀 I’m interested in app development, JavaScript, and creating interactive learning experiences.
+- 🌱 I’m currently learning  advanced app development techniques  and exploring new features for my projects.
+- 💞️ I’m looking to collaborate on collaborate on creative app projects, especially those that involve educational tools or innovative tech solutions.
+- 📫 How to reach me: nafula.charity355@gmail.com
+- 😄 Pronouns: She/Her
+- ⚡ Fun fact:I laugh alot.😂😉Am shy at times.I love building connections and exploring new ways to make technology more accessible to everyone❤️
+👨‍💻👨‍💻👨‍💻
+<!---
+C-topaz/C-topaz is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
